d15175a2d6a85cc0b8846c33bb4bbe782678c857
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-04-02 22:17:06 +0530

    Update index.js

--- service/index.js
+++ service/index.js
@@ -10,7 +10,7 @@ const app = express();
 connectToDatabase();
 
 app.use(express.json())
-app.use(cors({ origin: ["http://localhost:3000/"] }));
+app.use(cors({ origin: "*" }));
 
 const upload = multer();
 app.use(upload.any());
